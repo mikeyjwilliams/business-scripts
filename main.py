1a40891cbefeba569d0dc1e2fc38e4e2c1622bec
michael williams <williamsmikeyj@outlook.com>
2022-07-05 14:01:49 -0400

    ongoing pulling data first search

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from utils import bs4_utility  # type: ignore
+from utils import functions
+from utils import Data
 
 
 def main():
-    bs4_utility.pull_soup()
+    first_search()
+    # soup = bs4_utility.scrape_website(
+    #     Data.PLASTICPLACE_TRASH_BAGS_SMALL_8_GAL_1000)
+    # print(soup.find('span', id='productTitle'))
+    # title = 'Plasticplace 8 Gallon Trash Bags │ 0.7 Mil │ White Drawstring Garbage Can Liners │ 22" x 22" (200 Case), Count       </span>'
+
+
+def first_search():
+    plasticplace_page = bs4_utility.scrape_website(
+        Data.PLASTICPLACE_TRASH_BAGS_SMALL_8_GAL_1000)
+    title = functions.get_title(plasticplace_page)
 
 
 if __name__ == "__main__":
